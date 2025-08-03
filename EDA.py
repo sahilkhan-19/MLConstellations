5d05212ed70d6dd9b90955605d6dcee3c9f4b40d
Sahil Khan <sahilkhan19info@gmail.com>
2025-08-03 15:25:08 +0530

    Initial commit with Python script and CSV data

---
+++ EDA.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Churn_Modelling.csv')
+df.info()
+print(df.isnull().sum())
+
+updated_df = df
+updated_df['Age'] = updated_df['Age'].fillna(df['Age'].mean())
+updated_df['CreditScore'] = updated_df['CreditScore'].fillna(df['CreditScore'].mean())
+df.info()
+print(df.isnull().sum())
