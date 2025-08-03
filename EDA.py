2e0c7cf19894a3018d14c6467cd74ae114cefe51
Sahil Khan <sahilkhan19info@gmail.com>
2025-08-03 15:34:26 +0530

    Added subfolder for datasets

--- EDA.py
+++ EDA.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('Churn_Modelling.csv')
+df = pd.read_csv('DataVerse/Churn_Modelling.csv')
 df.info()
 print(df.isnull().sum())
 
